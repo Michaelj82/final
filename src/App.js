f3e58afbe90bda82e8fa4407459bdc9e6fc45563
Michael <mjj.yoyogi@gmail.com>
2023-02-21 23:16:29 -0500

    tryna set up firebase users.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { loginEmailPassword } from "./firebase";
+
 
 function App() {
+
+  function handleSubmit (event){
+    event.preventDefault()
+
+    let email = event.target[0].value
+    let password = event.target[1].value
+
+    loginEmailPassword(email, password)
+
+  }
+
   return (
     <div>
-      yo
+      <form onSubmit={handleSubmit}>
+        <input name = {'email'} type={'email'} placeholder={'Email here'} required></input>
+        <input name = {'password'} type={'password'} required></input>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,30 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {
+getAuth,
+connectAuthEmulator,
+signInWithEmailAndPassword} from 'firebase/auth'
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAZDR0JIKMOxO5XMMrBk4Zh7Il8z3CBcNs",
+  authDomain: "final-f9da3.firebaseapp.com",
+  projectId: "final-f9da3",
+  storageBucket: "final-f9da3.appspot.com",
+  messagingSenderId: "380134219305",
+  appId: "1:380134219305:web:6c8cc305eff826a5748638",
+  measurementId: "G-SY49R865LC"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app)
+
+
+connectAuthEmulator(auth, "http://localhost:9099")
+
+export const loginEmailPassword = async (email, password) => {
+
+    const userCredential = await signInWithEmailAndPassword(auth, email, password)
+    console.log(userCredential.user);
+}
